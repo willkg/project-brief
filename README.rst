61574c8f722a1254bc4584524eb7c0eb65c93dac
Will Kahn-Greene <willkg@mozilla.com>
2017-11-03 11:37:43 -0400

    Project scaffolding

---
+++ README.rst
@@ -0,0 +1,33 @@
+=============
+project-brief
+=============
+
+project-brief has some library functions to make it easier to do project
+summaries and quarterly-rollups.
+
+:Code: https://github.com/willkg/project-brief
+:Issues: https://github.com/willkg/project-brief/issues
+:License: MPL v2
+:Documentation: None, yet
+
+
+Goals
+=====
+
+A bunch of us write summaries and project rollups based on data from GitHub and
+Bugzilla. It's a bunch of code we're writing and it'd be nice if we shared it.
+
+* support multiple information sources (Bugzilla, GitHub, etc)
+
+* single repo
+
+
+Install
+=======
+
+To install project-brief, run::
+
+    $ pip install https://github.com/willkg/project-brief/archive/master.zip
+
+
+There aren't any PyPI releases, yet.
---
+++ setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+import os
+import re
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        import shlex
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        pytest_args = shlex.split(self.pytest_args) if self.pytest_args else []
+        errno = pytest.main(pytest_args)
+        sys.exit(errno)
+
+
+def get_version():
+    fn = os.path.join('brief', '__init__.py')
+    vsre = r"""^__version__ = ['"]([^'"]*)['"]"""
+    version_file = open(fn, 'rt').read()
+    return re.search(vsre, version_file, re.M).group(1)
+
+
+def get_file(fn):
+    with open(fn) as fp:
+        return fp.read()
+
+
+setup(
+    name='project-brief',
+    version=get_version(),
+    description='Library for generating project summaries and roll-ups',
+    long_description=(
+        get_file('README.rst') + '\n\n' + get_file('HISTORY.rst')
+    ),
+    url='https://github.com/willkg/project-brief',
+    tests_requires=['pytest'],
+    packages=[
+        'brief',
+    ],
+    package_dir={
+        'brief': 'brief',
+    },
+    cmdclass={
+        'test': PyTest
+    },
+    include_package_data=True,
+    license="MPLv2",
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
